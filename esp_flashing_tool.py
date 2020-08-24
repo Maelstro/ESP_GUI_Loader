fe680e6a594038614e06621ff55a89916fccc169
Maciej Wójcik <maciej.wojcik.xperia@gmail.com>
2020-08-24 23:01:25 +0200

    Changed flashing from command prompt to esptool.py-driven
    Signed-off-by: Maelstro <maciej.wojcik.xperia@gmail.com>

--- esp_flashing_tool.py
+++ esp_flashing_tool.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 from window import MainWindow
+import sys
 
 if __name__ == '__main__':
     root = tk.Tk()
     app = MainWindow(root)
+    sys.stdout.write = app.redirector
+    sys.stderr.write = app.redirector
     root.mainloop()
--- window.py
+++ window.py
@@ -1,17 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
-from subprocess import Popen, PIPE
 import serial.tools.list_ports
 import threading
-import os
+import esptool
 
 class MainWindow(tk.Tk):
     def __init__(self, master):
         # Variables
-        self.flash_command = ""
         self.project_directory = ""
-        self.esp_tools_path = ""
         self.selected_port = ""
 
         self.root = master
@@ -24,57 +21,59 @@ class MainWindow(tk.Tk):
         self.main_text = tk.Label(self.root, text="ESP32 GUI Flasher")
         self.main_text.pack()
 
-        self.label_esp_path = tk.Label(self.root, text="ESP-IDF Tools path")
-        self.label_esp_path.place(relx=0.033, rely=0.022)
-
-        self.text_esp_path = tk.Text(self.root)
-        self.text_esp_path.place(relx=0.033, rely=0.067, relheight=0.053, relwidth=0.357)
-        self.text_esp_path.config(wrap='none')
-
         self.text_field = tk.Text(self.root)
         self.text_field.place(relx=0.033, rely=0.4, relheight=0.564, relwidth=0.933)
 
         self.label_proj_dir = tk.Label(self.root, text="Project directory")
-        self.label_proj_dir.place(relx=0.033, rely=0.156)
+        self.label_proj_dir.place(relx=0.033, rely=0.022)
 
         self.text_proj_dir = tk.Text(self.root)
-        self.text_proj_dir.place(relx=0.033, rely=0.2, relheight=0.053, relwidth=0.357)
+        self.text_proj_dir.place(relx=0.033, rely=0.067, relheight=0.053, relwidth=0.357)
         self.text_proj_dir.config(wrap='none')
 
-        self.button_esp_browse = tk.Button(self.root, text="Browse ESP-IDF Tools",
-                                           command=lambda: self.browse_path("esp_idf"))
-        self.button_esp_browse.place(relx=0.4, rely=0.067, height=24, width=127)
+        self.label_com_port = tk.Label(self.root, text="COM port with ESP32")
+        self.label_com_port.place(relx=0.033, rely=0.156)
+
+        self.text_com_port = tk.Text(self.root)
+        self.text_com_port.place(relx=0.033, rely=0.2, relheight=0.053, relwidth=0.357)
+        self.text_com_port.config(wrap='none')
 
         self.button_proj_browse = tk.Button(self.root, text="Browse project",
-                                            command=lambda: self.browse_path("project"))
-        self.button_proj_browse.place(relx=0.4, rely=0.2, height=24, width=127)
+                                            command=lambda: self.browse_project_path())
+        self.button_proj_browse.place(relx=0.4, rely=0.067, height=24, width=127)
+
+        self.button_detect_port = tk.Button(self.root, text="Detect ESP32",
+                                            command=lambda: self.autodetect_com_port())
+        self.button_detect_port.place(relx=0.4, rely=0.2, height=24, width=127)
 
         self.button_flash = tk.Button(self.root, text="Flash program", command=self._flash_wrapper)
         self.button_flash.place(relx=0.65, rely=0.067, height=84, width=157)
+        self.button_flash.config(state=tk.DISABLED)
 
-    def append_command(self, cmd: str):
-        if len(self.flash_command) < 1:
-            self.flash_command += cmd
-        else:
-            self.flash_command += (" && " + cmd)
+    def redirector(self, input_str):
+        self.text_field.insert(tk.INSERT, input_str)
+        self.text_field.see(tk.END)
 
     def autodetect_com_port(self):
         device_list = serial.tools.list_ports.comports()
         for device in device_list:
             if "CP210x" in device[1]:
                 self.selected_port = device[0]
+                self.text_com_port.delete("1.0", tk.END)
+                self.text_com_port.insert(tk.END, self.selected_port)
+                self.text_com_port.see(tk.END)
+                self.button_flash.config(state=tk.ACTIVE)
                 break
+        if self.selected_port == "":
+            msg = "Did not detect any ESP32!"
+            self.show_messagebox("No ESP32 detected", msg)
 
-    def browse_path(self, type: str):
-        if type == "project":
-            self.project_directory = filedialog.askdirectory()
-            self.text_proj_dir.insert(tk.END, self.project_directory)
-            self.text_proj_dir.see(tk.END)
-        elif type == "esp_idf":
-            self.esp_tools_path = filedialog.askdirectory()
-            self.text_esp_path.insert(tk.END, self.esp_tools_path)
-            self.text_esp_path.see(tk.END)
 
+    def browse_project_path(self):
+        self.project_directory = filedialog.askdirectory()
+        self.text_proj_dir.delete("1.0", tk.END)
+        self.text_proj_dir.insert(tk.END, self.project_directory)
+        self.text_proj_dir.see(tk.END)
 
     def show_text(self, text):
         self.text_field.insert(tk.END, text)
@@ -86,66 +85,25 @@ class MainWindow(tk.Tk):
         print(msg)
 
     def flash_program(self):
-        # Detect the COM port
-        self.autodetect_com_port()
-
-        if self.selected_port == "":
-            msg = "Did not detect any ESP32!"
-            self.show_messagebox("No ESP32 detected", msg)
-        else:
-            print(self.selected_port)
-            _cmd_flash = "idf.py -p " + self.selected_port + " flash"
-            print(_cmd_flash)
-
-            if self.esp_tools_path == "":
-                msg = "ESP-IDF Tools path is not set!"
-                self.show_messagebox("No ESP-IDF Tools set", msg)
-            elif "export.bat" not in os.listdir(self.esp_tools_path):
-                msg = "Export.bat not found, wrong ESP-IDF Tools directory!"
-                self.show_messagebox("Wrong ESP-IDF Tools directory", msg)
-            else:
-                self.esp_tools_path = self.esp_tools_path.replace("/", "\\")
-                _prefix = self.esp_tools_path[:2]
-                self.append_command(_prefix)
-                self.append_command(self.esp_tools_path + "\\export.bat")
-
-                if self.project_directory == "":
-                    msg = "Project directory is not set!"
-                    self.show_messagebox("Project directory not set", msg)
-
-                else:
-                    self.project_directory = self.project_directory.replace("/", "\\")
-                    _prefix = self.project_directory[:2]
-                    self.append_command(_prefix)
-                    self.append_command("cd " + self.project_directory)
-                    self.append_command("idf.py fullclean && idf.py build")
-                    self.append_command(_cmd_flash)
-
-                    print("Command: {}".format(self.flash_command))
-
-                    process = Popen(self.flash_command,
-                                    bufsize=4096,
-                                    stdout=PIPE,
-                                    stderr=PIPE,
-                                    shell=True)
-
-                    for line in process.stdout:
-                        self.show_text(line)
-
-                    streamdata = process.communicate()[0]
-                    rc = process.returncode
-                    print("Return code: {}".format(rc))
-
-                    if rc == 2:
-                        msg = "Error during flashing the program to the microcontroller. esptool.py not installed on " \
-                              "this Python version or wrong user permissions (must be 'Run as Administrator'). "
-                        messagebox.showerror(title='Error during flashing', message=msg)
-                    elif rc == 0:
-                        msg = "Program flashed correctly to the microcontroller."
-                        messagebox.showinfo(title="Finished without errors", message=msg)
-
+        self.button_flash.config(state=tk.DISABLED)
+        flash_command = []
+        flash_command.extend(['--baud', '460800', '--port', self.selected_port, 'write_flash'])
+        try:
+            with open(self.project_directory + '/flash_project_args') as f:
+                args = []
+                lines = f.read().split('\n')
+                for i, line in enumerate(lines):
+                    split_line = line.split(' ')
+                    if 0 < i < len(lines) - 1:
+                        split_line[1] = self.project_directory + '/' + split_line[1]
+                        args.extend(split_line)
+            flash_command.extend(args)
+            esptool.main(flash_command)
+        except FileNotFoundError:
+            msg = ("No flash_project_args found! "
+                   "Change the project directory for the one that has flash_project_args in it.")
+            self.show_messagebox("No flash_project_args found", msg)
         # Clear command
-        self.flash_command = ""
         self.button_flash.config(state=tk.ACTIVE)
 
     def _flash_wrapper(self):
